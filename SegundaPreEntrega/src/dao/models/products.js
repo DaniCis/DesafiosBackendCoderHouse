9cbbd572a699a76f96c0ef97e14035d297426f6b
DaniCis <danicisneros2@hotmail.com>
2023-07-16 23:09:36 -0500

    get products con paginate

--- SegundaPreEntrega/src/dao/models/products.js
+++ SegundaPreEntrega/src/dao/models/products.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import mongoosePaginate from 'mongoose-paginate-v2';
 
 const productsCollection = 'products'
 const productsSchema = mongoose.Schema({
@@ -18,5 +19,6 @@ const productsSchema = mongoose.Schema({
     thumbnails: Array,
 })
 
+productsSchema.plugin(mongoosePaginate)
 const productsModel = mongoose.model(productsCollection, productsSchema)
 export default productsModel;
