f6123abc1da0c9bfa4093e5f047961dc00edcf0c
DaniCis <danicisneros2@hotmail.com>
2023-08-24 20:59:21 -0500

    services completed

--- Desafio7 - ServerRestr/src/services/messages.service.js
+++ Desafio7 - ServerRestr/src/services/messages.service.js
@@ -1,10 +1,47 @@
+import messagesModel from '../model/messages.model.js'
+
 class MessageService{
-    async getMessages(){
+    getMessages = async()=>{
         try{
-           // const response= await toyModel.find()
-            console.log(response)
-            return response
+            const messages = await messagesModel.find().lean()
+            return messages
         }catch{
+            throw new Error(error.message)
+        }
+    }
+
+    getMessageById = async(id)=>{
+        try{
+            let message = await messagesModel.find({_id: id})
+            return message;
+        }catch(e){
+            throw new Error(error.message)
+        }
+    }
+    
+    createMessage = async()=>{
+        try{
+            let result = await messagesModel.create(message)
+            return result;
+        }catch(e){
+            throw new Error(error.message)
+        }
+    }
+    
+    updateMessage = async(id,message)=>{
+        try{
+            let result = await messagesModel.updateOne({_id: id}, message)
+            return result
+        }catch(e){
+            throw new Error(error.message)
+        }
+    }
+    
+    deleteMessage = async(id)=>{
+        try{
+            let message = await messagesModel.deleteOne({_id: id})
+            return message
+        }catch(e){
             throw new Error(error.message)
         }
     }
--- Desafio7 - ServerRestr/src/services/products.service.js
+++ Desafio7 - ServerRestr/src/services/products.service.js
@@ -1,4 +1,43 @@
-class ProductService{
+import productsModel from '../model/products.model.js'
 
+class ProductService{
+    getProducts = async()=>{
+        try{
+
+        }catch(e){
+            throw new Error(error.message)
+        }
+    }
+    getProductById = async(id)=>{
+        try{
+
+        }catch(e){
+            throw new Error(error.message)
+        }
+    }
+
+    createProduct = async()=>{
+        try{
+
+        }catch(e){
+            throw new Error(error.message)
+        }
+    }
+
+    updateProduct = async()=>{
+        try{
+
+        }catch(e){
+            throw new Error(error.message)
+        }
+    }
+    
+    deleteProduct = async() =>{
+        try{
+
+        }catch(e){
+            throw new Error(error.message)
+        }
+    }
 }
 export default new ProductService()
