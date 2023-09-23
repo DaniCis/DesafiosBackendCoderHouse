1ca1da704db06ab4cba1d9d9ec6c7c81cecefe84
DaniCis <danicisneros2@hotmail.com>
2023-09-23 17:54:19 -0500

    estructura practica 3

---
+++ PracticaIntegradora3/src/config/config.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv"
+
+const environment = "DEVELOPMENT"
+
+dotenv.config({
+    path: environment === "PRODUCTION" ? './.env.production' : './.env.development'
+})
+
+const CONFIG ={
+    ENVIRONMENT: environment,
+    MONGO_URI: process.env.MONGO_URI || '',
+    PORT: process.env.PORT || 3000,
+    DATASOURCE: process.env.PERSISTENCE || 'MONGO',
+    ADMIN_USERNAME: process.env.ADMIN_USERNAME,
+    ADMIN_PASS: process.env.ADMIN_PASS,
+    GIT_CLIENT_ID: process.env.GIT_CLIENT_ID,
+    GIT_CLIENT_SECRET:process.env.GIT_CLIENT_SECRET,
+}
+export default CONFIG;
