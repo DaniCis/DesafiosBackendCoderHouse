011e392b1b3dfd9bb923a9fc024b2bf2ef6a33f8
DaniCis <danicisneros2@hotmail.com>
2023-11-02 22:56:33 -0500

    Estructura desafio final

---
+++ DesafioFinal/src/routers/users.router.js
@@ -0,0 +1,15 @@
+import {Router} from 'express'
+import UsersController from '../controllers/users.controllers.js'
+import usersControllers from '../controllers/users.controllers.js'
+
+const router = Router ()
+
+router.get('/', UsersController.getUsers)
+router.get('/:uid', UsersController.getUserById)
+router.post('/', UsersController.createUser)
+router.put('/uid', UsersController.updateUser)
+router.delete('/:uid',UsersController.deleteUser)
+router.post('/premium/:uid',UsersController.swapUserRole)
+router.get('/documents', usersControllers.getUserDocuments)
+
+export default router
